1b59abe67039fe020fb6724565b24e76d8967e20
Tedhoon <gt0305@likelion.org>
2019-09-14 22:43:18 +0900

    중간

--- mystorage/serializers.py
+++ mystorage/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 
 class EssaySerializer(serializers.ModelSerializer):
 
+    author_name = serializers.ReadOnlyField(source='author.username')
+
     class Meta:
         model = Essay
-        fields = '__all__'
+        fields = ('id' , 'title' , 'body' , 'author_name')
--- mystorage/views.py
+++ mystorage/views.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets ,serializers
 from .models import Essay 
 from .serializers import EssaySerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Essay.objects.all()
     serializer_class = EssaySerializer
+
+    def perform_create(self, serializer): # 자동으로 author_name을 등록
+        serializer.save(author = self.request.user)
